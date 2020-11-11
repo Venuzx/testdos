6b63c0193025415fccbcc7fe8f6d33f490bff798
Venuzx <74275619+Venuzx@users.noreply.github.com>
2020-11-11 17:48:14 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+ScrollReveal().reveal('.portafolio', { delay: 300 });
+ScrollReveal().reveal('.clientes contenedor', { delay: 300 });
+ScrollReveal().reveal('.cards', { delay: 300 });
+ScrollReveal().reveal('.about-services', { delay: 300 });
+ScrollReveal().reveal('.contenedor', { delay: 300 });
+ScrollReveal().reveal('.contenedor-footer', { delay: 300 });
+ScrollReveal().reveal('.content-foo', { delay: 300 });
+ScrollReveal().reveal('.titulo-final', { delay: 300 });
